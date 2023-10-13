dfd6458de1a1b329671a8f583fd47ecaf7dc11c2
krystianklimczak <krystianklimczak1996r.com>
2023-10-13 20:42:12 +0200

    import user and profile component and draw it in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import Profile from './profile/Profile';
+import user from './profile/user';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 };
