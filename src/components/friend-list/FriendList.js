72822abb593ce5e5b64ce49723a534bc6ba96445
krystianklimczak <krystianklimczak1996r.com>
2023-10-13 23:32:37 +0200

    implement friendlist component with css modules and friends.json

---
+++ src/components/friend-list/FriendList.js
@@ -0,0 +1,35 @@
+import css from "./FriendList.module.css";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+
+export default function FriendList({friends}) {
+    return (
+        <ul className={css["friends-list"]}>
+            {friends.map(({avatar, name, isOnline, id}) => (
+                <FriendListItem
+                 avatar={avatar}
+                 name={name}
+                 isOnline={isOnline}
+                 key={id}
+                />
+            ))}
+        </ul>
+    )
+}
+
+function FriendListItem({avatar, name, isOnline}) {
+    return (
+        <li 
+         className={css.item}
+        >
+            <span className={clsx([css.status], isOnline && [css.isOnline])}></span>
+            <img
+             className={css.avatar}
+             src={avatar}
+             alt="User avatar"
+             width="48"
+            />
+            <p className={css.name}>{name}</p>
+        </li>
+    )
+}
