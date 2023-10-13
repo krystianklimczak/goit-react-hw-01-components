cc90a393971b08584c263e054d2cc804298c60d0
krystianklimczak <krystianklimczak1996r.com>
2023-10-13 22:16:43 +0200

    import data and statistics component and draw it in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
-import Profile from './profile/Profile';
 import user from './profile/user';
+import data from './statistics/data';
+import Profile from './profile/Profile';
+import Statistics from './statistics/Statistics';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics
+       title="upload stats"
+       stats={data}
+      />
+
+    </>
   );
 };
