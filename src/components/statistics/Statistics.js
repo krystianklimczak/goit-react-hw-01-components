fb889a967dd01a861bf6e7b309a50067aaaae0ee
krystianklimczak <krystianklimczak1996r.com>
2023-10-13 22:33:10 +0200

    add propTypes to all props

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,4 +1,5 @@
 import css from "./Statistics.module.css";
+import PropTypes from "prop-types";
 
 const getRandomColor = () => {
     return `#${Math.floor(Math.random()*16777215).toString(16)}`;
@@ -27,3 +28,12 @@ export default function Statistics({title, stats}) {
         </section>
     )
 }
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+    })),
+  };
