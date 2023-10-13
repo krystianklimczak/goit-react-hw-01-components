63c63a3d1ee055894b8dd5de9c0fdc547078a663
krystianklimczak <krystianklimczak1996r.com>
2023-10-13 22:19:23 +0200

    implement statistics component with css modules, data.json and random color fn

---
+++ src/components/statistics/Statistics.js
@@ -0,0 +1,29 @@
+import css from "./Statistics.module.css";
+
+const getRandomColor = () => {
+    return `#${Math.floor(Math.random()*16777215).toString(16)}`;
+}
+
+export default function Statistics({title, stats}) {
+    return (
+        <section className={css.statistics}>
+
+            {title && <h2 className={css.title}>{title}</h2>}
+
+            <ul className={css["stat-list"]}>
+                {stats.map(stat => (
+                    <li 
+                    className={css.item}  
+                    key={stat.id} 
+                    style={{
+                        backgroundColor: getRandomColor(),
+                    }}>
+                        <span className={css.label}>{stat.label}</span>
+                        <span className={css.percentage}>{stat.percentage}%</span>
+                    </li>
+                ))}
+            </ul>
+
+        </section>
+    )
+}
