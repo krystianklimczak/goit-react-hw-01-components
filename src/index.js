d6a0dc369b751b226a88cafa0403391223affc25
krystianklimczak <krystianklimczak1996r.com>
2023-10-14 03:47:25 +0200

    fix styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import './materialize.js';
 import M from 'materialize-css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
